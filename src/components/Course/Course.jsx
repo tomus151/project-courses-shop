374a894c85b811253e69c92a3b756810dcb2e6d9
tomus151 <ciasteczkowy15@gmail.com>
2021-12-17 18:14:22 +0100

    feat: buy button for courses

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { StoreContext } from '../../store/StoreProvider';
+import request from '../../helpers/request'
 import bemCssModules from 'bem-css-modules';
 import { default as CourseStyles } from './Course.module.scss';
+import { useHistory } from 'react-router-dom';
 
 const style = bemCssModules(CourseStyles)
-const Course = ({ authors, img, price, title }) => {
+const Course = ({ authors, id, isUserContext = false, img, price, title }) => {
+    const { user, setUser } = useContext(StoreContext)
+    const history = useHistory()
     const allAuthors = authors.join(', ');
-    console.log(img)
+    const isUserLogged = Boolean(user);
+    const handleOnClick = async () => {
+        try {
+            const { data, status } = await request.patch(
+                '/users',
+                {
+                    login: user.login,
+                    courseId: id
+                }
+            )
+            if (status === 202) {
+                setUser(data.user);
+                history.push('/my-courses');
+            }
+        } catch (error) {
+            console.warn(error)
+        }
+    }
+    const shouldBeBuyButtonVisible = isUserLogged && !isUserContext;
+    console.log(isUserLogged)
+    console.log(!isUserContext)
     return (
         <li>
             <article className={style()}>
@@ -13,6 +38,7 @@ const Course = ({ authors, img, price, title }) => {
                 <img className={style('image')} src={img} alt="title" />
                 <p className={style('price')}>{`Koszt kursu: ${price}zł`}</p>
                 <p className={style('authors')}>{`Autorzy kursu: ${allAuthors}`}</p>
+                {shouldBeBuyButtonVisible && <button onClick={handleOnClick}>Zakup ten kurs</button>}
             </article>
         </li>
     );
--- src/components/UserCourses/UserCourses.jsx
+++ src/components/UserCourses/UserCourses.jsx
@@ -6,7 +6,7 @@ import { default as UserCoursesStyle } from './UserCourses.module.scss';
 const style = bemCssModules(UserCoursesStyle);
 const UserCourses = () => {
     const { user, courses } = useContext(StoreContext);
-    const buyedCourses = courses.filter(course => user.courses.includes(course.id)).map(course => <Course key={course.id} {...course} />)
+    const buyedCourses = courses.filter(course => user.courses.includes(course.id)).map(course => <Course key={course.id} isUserContext={true} {...course} />)
     return (
         <section className={style()}>
             <h2 className={style('title')}>Twoje wykupione kursy</h2>
